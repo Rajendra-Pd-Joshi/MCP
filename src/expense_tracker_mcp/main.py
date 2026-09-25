5111c01dbc212f273360e7483f7a7a28ffd71de7
Rajendra Prasad Joshi <rajendraprasadjoshi22@gmail.com>
2026-09-25 15:27:06 +0530

    learning how mcp works

---
+++ src/expense_tracker_mcp/main.py
@@ -0,0 +1,21 @@
+import random
+from fastmcp import FastMCP
+
+# Create a FastMcp server instance
+
+mcp = FastMCP(name = 'demo server')
+
+@mcp.tool
+def roll_dice(n_dice: int = 1)-> list[int]:
+    """Roll n_dice 6-sided and return the results."""
+
+    return [random.randint(1,6) for _ in range(n_dice)]
+
+
+@mcp.tool
+def add_numbers(a: float, b: float)->float:
+    """Add two numbers together"""
+    return a+b
+
+if __name__ == '__main__':
+    mcp.run()
